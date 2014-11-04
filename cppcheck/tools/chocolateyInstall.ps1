a2316b4fba8e66a2cb1c39f94f6943c52e5bb0d3
Matthieu Penant <thieum22@hotmail.com>
2014-11-04 14:46:51 -0500

    Initial commit of cppcheck 1.67

---
+++ cppcheck/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'cppcheck' 
+$installerType = 'MSI'
+$url = 'http://downloads.sourceforge.net/project/cppcheck/cppcheck/1.67/cppcheck-1.67-x86-Setup.msi' 
+$silentArgs = '/quiet' 
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+
