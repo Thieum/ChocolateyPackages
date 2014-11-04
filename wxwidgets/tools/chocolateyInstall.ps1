cf6df985f5ac9c6f8207d821c70027a75d0aa527
Matthieu Penant <thieum22@hotmail.com>
2014-11-04 15:36:41 -0500

    initial commit of wxwidgets 3.0.1

---
+++ wxwidgets/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'wxwidgets' 
+$installerType = 'EXE' 
+$url = 'http://sourceforge.net/projects/wxwindows/files/3.0.1/wxMSW-3.0.1-Setup.exe' 
+$silentArgs = '/silent' 
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
