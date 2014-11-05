097faf8389bd5779759ebae41a87ae944265de0b
Matthieu Penant <thieum22@hotmail.com>
2014-11-05 10:16:53 -0500

    initial commit productivitypowertools2012

---
+++ productivitypowertools2012/tools/chocolateyInstall.ps1
@@ -0,0 +1,4 @@
+﻿$id = "productivitypowertools2012"
+$url = "https://visualstudiogallery.msdn.microsoft.com/3a96a4dc-ba9c-4589-92c5-640e07332afd/file/88404/3/ProPowerTools.vsix"
+
+Install-ChocolateyVsixPackage $id $url
