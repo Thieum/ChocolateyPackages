d1d944c96182ddd240b17cc1a4cc3d0547b6050c
Matthieu Penant <thieum22@hotmail.com>
2014-11-05 10:34:56 -0500

    initial commit slow cheetah

---
+++ slowcheetah/tools/chocolateyInstall.ps1
@@ -0,0 +1,4 @@
+﻿$id = "slowcheetah"
+$url = "https://visualstudiogallery.msdn.microsoft.com/69023d00-a4f9-4a34-a6cd-7e854ba318b5/file/55948/26/SlowCheetah.vsix"
+
+Install-ChocolateyVsixPackage $id $url
