2ab60b8493860abab6167fb75a134715dc5bf26f
Matthieu Penant <thieum22@hotmail.com>
2014-11-19 13:32:30 -0500

    Visual Leak Detector

---
+++ visualleakdetector/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'visualleakdetector' 
+$installerType = 'EXE'
+$url = 'http://vld.codeplex.com/downloads/get/670164' # download url
+$silentArgs = '/S' 
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
