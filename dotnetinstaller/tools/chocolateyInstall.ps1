7062500cfb3be862170c2f4c6664b1cfe4a7340f
Matthieu Penant <thieum22@hotmail.com>
2014-11-27 11:07:46 -0500

    dotnetinstaller initial commit

---
+++ dotnetinstaller/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'dotnetinstaller' 
+$installerType = 'MSI'
+$url = 'http://code.dblock.org/downloads/dotnetinstaller/dotNetInstaller.2.3.msi' 
+$silentArgs = '/quiet' 
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+
