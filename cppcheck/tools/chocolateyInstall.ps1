3b598902935c3eb505ca6123ed6f05825c7ab379
Matthieu Penant <thieum22@hotmail.com>
2015-01-08 10:13:43 -0500

    MAJ cppcheck 1.68
    MAJ cppcheck 1.68

--- cppcheck/tools/chocolateyInstall.ps1
+++ cppcheck/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'cppcheck' 
 $installerType = 'MSI'
-$url = 'http://downloads.sourceforge.net/project/cppcheck/cppcheck/1.67/cppcheck-1.67-x86-Setup.msi' 
+$url = 'http://downloads.sourceforge.net/project/cppcheck/cppcheck/1.68/cppcheck-1.68-x86-Setup.msi' 
 $silentArgs = '/quiet' 
 $validExitCodes = @(0)
 
