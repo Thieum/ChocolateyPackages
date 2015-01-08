ea6bd1dc5d8b8231fd75f9b4da14408788237738
Matthieu Penant <thieum22@hotmail.com>
2015-01-08 11:09:31 -0500

    staruml2 temp commit

---
+++ staruml2/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'cppcheck' 
+$installerType = 'MSI'
+$url = 'http://staruml.io/download/release/v2.0.0/StarUML-v2.0.0.msi' 
+$silentArgs = '/quiet' 
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+
