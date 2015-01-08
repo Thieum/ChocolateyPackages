99909dae30ba56bf884101685b75c08882dfe106
Matthieu Penant <thieum22@hotmail.com>
2015-01-08 11:24:34 -0500

    staruml2 fixes

--- staruml2/tools/chocolateyInstall.ps1
+++ staruml2/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
-﻿$packageName = 'cppcheck' 
+﻿$packageName = 'staruml2' 
 $installerType = 'MSI'
 $url = 'http://staruml.io/download/release/v2.0.0/StarUML-v2.0.0.msi' 
-$silentArgs = '/quiet' 
+$silentArgs = '/qn' 
 $validExitCodes = @(0)
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
