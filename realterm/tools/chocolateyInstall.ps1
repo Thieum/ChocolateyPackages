8672f3f5d1ebce00b00638dbf68be67104b3541c
Matthieu Penant <thieum22@hotmail.com>
2015-01-08 14:19:12 -0500

    realterm - initial commit

---
+++ realterm/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'realterm' 
+$installerType = 'EXE'
+$url = 'http://sourceforge.net/projects/realterm/files/Realterm/2.0.0.70/Realterm_2.0.0.70_Signed_Wrapper_setup.exe' 
+$silentArgs = '/S' 
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+
