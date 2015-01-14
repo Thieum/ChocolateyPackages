b2f07521ae94733efda4b1ccef4e05513f951587
Matthieu Penant <thieum22@hotmail.com>
2015-01-14 16:52:05 -0500

    JustDecompile

---
+++ justdecompile/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'justdecompile' 
+$installerType = 'EXE'
+$url = 'https://www.telerik.com/downloads/productfiles/btmba/TelerikJustDecompileSetup_2014.3.1021.0.exe' 
+$silentArgs = '/quiet' 
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+
