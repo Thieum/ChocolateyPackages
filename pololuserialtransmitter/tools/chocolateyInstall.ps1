d63affba1f1335f2662917a961a55793a57f6885
Matthieu Penant <thieum22@hotmail.com>
2015-01-14 16:03:20 -0500

    Pololu 1.3

---
+++ pololuserialtransmitter/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿$packageName = 'pololuserialtransmitter' 
+$installerType = 'EXE'
+$url = 'https://www.pololu.com/file/download/PololuSerialTransmitterV1.3.zip?file_id=0J132' 
+$silentArgs = '' 
+$validExitCodes = @(0)
+$destinationPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$setupFile = Join-Path $destinationPath "setup.exe"
+
+Install-ChocolateyZipPackage "$packageName" $url $destinationPath
+
+Install-ChocolateyInstallPackage "$packageName" "$installerType" $setupFile
+
