fc0cfe042b8788693b001c8d6cbdd482f50ef62b
Matthieu Penant <thieum22@hotmail.com>
2015-01-15 10:57:08 -0500

    sqlserver data tools 2012

---
+++ sqlserverdatatools2012/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'sqlserverdatatools2012' 
+$installerType = 'EXE'
+$url = 'http://go.microsoft.com/fwlink/?LinkID=518798' 
+$silentArgs = '/q' 
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+
