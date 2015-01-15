20537f8a0979ec2c5864a853b4773f87bbc3f93e
Matthieu Penant <thieum22@hotmail.com>
2015-01-15 12:08:22 -0500

    justdecompile silent install testing
    need more info - asked on forums :
    http://www.telerik.com/forums/silent-install-for-chocolatey-package

--- justdecompile/tools/chocolateyInstall.ps1
+++ justdecompile/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'justdecompile' 
 $installerType = 'EXE'
 $url = 'https://www.telerik.com/downloads/productfiles/btmba/TelerikJustDecompileSetup_2014.3.1021.0.exe' 
-$silentArgs = '/quiet' 
+$silentArgs = '/S' 
 $validExitCodes = @(0)
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
