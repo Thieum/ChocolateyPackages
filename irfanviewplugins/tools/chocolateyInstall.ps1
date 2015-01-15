805e5f2c75f471b1902c5bedb182c9ba7cda9eb1
Matthieu Penant <thieum22@hotmail.com>
2015-01-15 12:08:50 -0500

    Irfanview plugins
    doesn't seem to work with chocolatey

---
+++ irfanviewplugins/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'irfanviewplugins' 
+$installerType = 'EXE'
+$url = 'http://www.tucows.com/download/windows/files/irfanview_plugins_438_setup.exe' 
+$silentArgs = "/silent"
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+
