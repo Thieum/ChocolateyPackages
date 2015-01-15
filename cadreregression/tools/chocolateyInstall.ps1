dee0a516dc4b5a71cf11a9fe84c90602b6fdaffb
Matthieu Penant <thieum22@hotmail.com>
2015-01-15 14:52:39 -0500

    CADRE Regression - test different switch silent

--- cadreregression/tools/chocolateyInstall.ps1
+++ cadreregression/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'cadreregression' 
 $installerType = 'EXE'
 $url = 'http://www.cadreanalytic.com/Files/CadRegrs.exe' 
-$silentArgs = "/silent"
+$silentArgs = "/quiet"
 $validExitCodes = @(0)
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
