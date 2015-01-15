9eda679996f0e5e4df8136e2e27d6b0e16a9d23a
Matthieu Penant <thieum22@hotmail.com>
2015-01-15 13:24:03 -0500

    camstudio

---
+++ camstudio/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'camstudio' 
+$installerType = 'EXE'
+$url = 'http://sourceforge.net/projects/camstudio/files/stable/CamStudio_2.7_r316_setup.exe' 
+$silentArgs = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+
