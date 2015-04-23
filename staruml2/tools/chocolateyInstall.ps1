f49a29e2674d7d158a0fe52ba16d216a67e0a28e
Matthieu Penant <thieum22@hotmail.com>
2015-04-23 09:15:00 -0400

    staruml2 2.1.3

--- staruml2/tools/chocolateyInstall.ps1
+++ staruml2/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'staruml2' 
 $installerType = 'MSI'
-$url = 'http://staruml.io/download/release/v2.0.2/StarUML-v2.0.2.msi' 
-$silentArgs = '/qn' 
+$url = 'http://staruml.io/download/release/v2.1.3/StarUML-v2.1.3.msi' 
+$silentArgs = '/qn /norestart' 
 $validExitCodes = @(0)
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
