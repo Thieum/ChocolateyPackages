66bf4b487b0655f99d9952cdba4631c775ed42ef
Matthieu Penant <thieum22@hotmail.com>
2016-01-11 08:34:07 -0500

    cppcheck 1.71

--- cppcheck/tools/chocolateyInstall.ps1
+++ cppcheck/tools/chocolateyInstall.ps1
@@ -1,8 +1,9 @@
 ﻿$packageName = 'cppcheck' 
 $installerType = 'MSI'
-$url = 'http://downloads.sourceforge.net/project/cppcheck/cppcheck/1.70/cppcheck-1.70-x86-Setup.msi' 
+$url = 'http://sourceforge.net/projects/cppcheck/files/cppcheck/1.71/cppcheck-1.71-x86-Setup.msi' 
+$urlX64 = 'http://sourceforge.net/projects/cppcheck/files/cppcheck/1.71/cppcheck-1.71-x64-Setup.msi' 
 $silentArgs = '/quiet' 
 $validExitCodes = @(0)
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+ 
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$urlX64" -validExitCodes $validExitCodes
 
