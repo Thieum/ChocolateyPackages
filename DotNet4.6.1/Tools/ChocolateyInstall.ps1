f3d40463fe98c5a8f24d11055acc587b9b206681
Matthieu Penant <thieum22@hotmail.com>
2016-01-11 08:34:25 -0500

    Dotnet 4.6.1 fixes

--- DotNet4.6.1/Tools/ChocolateyInstall.ps1
+++ DotNet4.6.1/Tools/ChocolateyInstall.ps1
@@ -1,6 +1,6 @@
 Function IsInstalled {
     $ver = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full').Release
-    return (!($ver -eq $null) -and ($ver -ge 394271))
+    return (!($ver -eq $null) -and ($ver -ge 394254))
 }
 
 if (IsInstalled) {
@@ -9,7 +9,7 @@ if (IsInstalled) {
 else {
     $packageName = 'DotNet461'
     $installerType = 'exe'
-    $Url = 'https://download.microsoft.com/download/3/5/9/35980F81-60F4-4DE3-88FC-8F962B97253B/NDP461-KB3102438-Web.exe'
+    $Url = 'https://download.microsoft.com/download/E/4/1/E4173890-A24A-4936-9FC9-AF930FE3FA40/NDP461-KB3102436-x86-x64-AllOS-ENU.exe'
     $silentArgs = "/Passive /NoRestart /Log ""$env:temp\net461.log"""
     $validExitCodes = @(
         0, # success
