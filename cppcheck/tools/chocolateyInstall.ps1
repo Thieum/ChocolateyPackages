a10d510dcc19ec11cbb775fd4927e934db417d1e
Matthieu Penant <thieum22@hotmail.com>
2016-06-17 11:23:02 -0400

    cppcheck 1.74

--- cppcheck/tools/chocolateyInstall.ps1
+++ cppcheck/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'cppcheck' 
 $installerType = 'MSI'
-$url = 'http://github.com/danmar/cppcheck/releases/download/1.73/cppcheck-1.73-x86-Setup.msi' 
-$urlX64 = 'http://github.com/danmar/cppcheck/releases/download/1.73/cppcheck-1.73-x64-Setup.msi' 
+$url = 'https://github.com/danmar/cppcheck/releases/download/1.74/cppcheck-1.74-x86-Setup.msi' 
+$urlX64 = 'https://github.com/danmar/cppcheck/releases/download/1.74/cppcheck-1.74-x64-Setup.msi' 
 $silentArgs = '/quiet' 
 $validExitCodes = @(0)
  
