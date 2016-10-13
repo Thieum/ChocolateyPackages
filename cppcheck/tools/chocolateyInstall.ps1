25e41a5e593f3414ab8dc8cf397aa0b9c42365f9
Matthieu Penant <thieum22@hotmail.com>
2016-10-13 10:41:20 -0400

    added checksums to cppcheck 1.75

--- cppcheck/tools/chocolateyInstall.ps1
+++ cppcheck/tools/chocolateyInstall.ps1
@@ -1,9 +1,23 @@
-﻿$packageName = 'cppcheck' 
-$installerType = 'MSI'
-$url = 'http://github.com/danmar/cppcheck/releases/download/1.75/cppcheck-1.75-x86-Setup.msi' 
-$urlX64 = 'http://github.com/danmar/cppcheck/releases/download/1.75/cppcheck-1.75-x64-Setup.msi' 
-$silentArgs = '/quiet' 
-$validExitCodes = @(0)
+﻿$ErrorActionPreference = 'Stop';
+$packageName = 'cppcheck'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://github.com/danmar/cppcheck/releases/download/1.75/cppcheck-1.75-x86-Setup.msi' 
+$url64 = 'https://github.com/danmar/cppcheck/releases/download/1.75/cppcheck-1.75-x64-Setup.msi' 
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI' 
+  url           = $url
+  url64bit      = $url64
+  softwareName  = 'cppcheck'
+  checksum      = 'A472D7871DD46CA660F54607199E466D5B831EA65255A749C0CBCE75100485DF'
+  checksumType  = 'sha256' 
+  checksum64    = 'C38A9FA53942825F8D919C6E8732E4424752EA793F679B5B759ADC6B6C3681E2'
+  checksumType64= 'sha256' 
+  silentArgs = '/quiet' 
+  validExitCodes = @(0)
+} 
  
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$urlX64" -validExitCodes $validExitCodes
+Install-ChocolateyPackage @packageArgs
 
