ecda1f2785f9e26d00411c0852c03ea69a42302f
Matthieu Penant <thieum22@hotmail.com>
2016-12-13 15:15:40 -0500

    staruml 2.8.0 + checksum

--- staruml2/tools/chocolateyInstall.ps1
+++ staruml2/tools/chocolateyInstall.ps1
@@ -1,8 +1,18 @@
-﻿$packageName = 'staruml2' 
-$installerType = 'MSI'
-$url = 'http://staruml.io/download/release/v2.7.0/StarUML-v2.7.0.msi' 
-$silentArgs = '/qn /norestart' 
-$validExitCodes = @(0)
+﻿$ErrorActionPreference = 'Stop';
+$packageName = 'staruml2'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'http://staruml.io/download/release/v2.8.0/StarUML-v2.8.0.msi' 
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
-
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI' 
+  url           = $url
+  softwareName  = 'staruml2'
+  checksum      = '9DC854E6230D20AAF69FEF6FEF39322A22B415145014BA4856C41A3F2256E222'
+  checksumType  = 'sha256' 
+  silentArgs = '/quiet' 
+  validExitCodes = @(0)
+} 
+ 
+Install-ChocolateyPackage @packageArgs
