e341511184632ca5ab9f6aefa665ccae11fc9459
Matthieu Penant <thieum22@hotmail.com>
2016-12-13 15:45:28 -0500

    DotNet 4.6.1 checksum

--- DotNet4.6.1/Tools/ChocolateyInstall.ps1
+++ DotNet4.6.1/Tools/ChocolateyInstall.ps1
@@ -8,15 +8,25 @@ if (IsInstalled) {
 }
 else {
     $packageName = 'DotNet461'
-    $installerType = 'exe'
-    $Url = 'https://download.microsoft.com/download/E/4/1/E4173890-A24A-4936-9FC9-AF930FE3FA40/NDP461-KB3102436-x86-x64-AllOS-ENU.exe'
-    $silentArgs = "/q /norestart /log ""$env:temp\net461.log"""
-    $validExitCodes = @(
-        0, # success
-        3010 # success, restart required
-    )
+    $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+    $url = 'https://download.microsoft.com/download/E/4/1/E4173890-A24A-4936-9FC9-AF930FE3FA40/NDP461-KB3102436-x86-x64-AllOS-ENU.exe' 
 
-    Install-ChocolateyPackage $packageName $installerType $silentArgs $Url -validExitCodes $validExitCodes
+    $packageArgs = @{
+    packageName   = $packageName
+    unzipLocation = $toolsDir
+    fileType      = 'exe' 
+    url           = $url
+    softwareName  = 'DotNet461'
+    checksum      = 'BEAA901E07347D056EFE04E8961D5546C7518FAB9246892178505A7BA631C301'
+    checksumType  = 'sha256' 
+    silentArgs = "/q /norestart /log ""$env:temp\net461.log"""
+    validExitCodes = @(
+            0, # success
+            3010 # success, restart required
+        )
+    } 
+    
+    Install-ChocolateyPackage @packageArgs
 
     if (-Not (IsInstalled)) {
         Write-Host "A restart is required to finalise the Microsoft .NET Framework 4.6.1 installation"
