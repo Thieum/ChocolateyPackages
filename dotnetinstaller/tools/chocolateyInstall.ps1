3fc43b7f6d62339ee5229399195a17dcc0bebeaa
Matthieu Penant <thieum22@hotmail.com>
2017-03-16 10:26:52 -0400

    dotnetinstaller 2.4cpush do

--- dotnetinstaller/tools/chocolateyInstall.ps1
+++ dotnetinstaller/tools/chocolateyInstall.ps1
@@ -1,8 +1,18 @@
-﻿$packageName = 'dotnetinstaller' 
-$installerType = 'MSI'
-$url = 'http://code.dblock.org/downloads/dotnetinstaller/dotNetInstaller.2.3.msi' 
-$silentArgs = '/quiet' 
-$validExitCodes = @(0)
+﻿$ErrorActionPreference = 'Stop';
+$packageName = 'dotnetinstaller'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://github.com/dblock/dotnetinstaller/releases/download/2.4/dotNetInstaller.2.4.msi' 
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
-
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI' 
+  url           = $url
+  softwareName  = 'dotnetinstaller'
+  checksum      = 'CE750FBC872EF177EFCE9DD07A5A5BAE6C3E85163424D6634BAAD0DC1AF5CF2E'
+  checksumType  = 'sha256' 
+  silentArgs = '/quiet' 
+  validExitCodes = @(0)
+} 
+ 
+Install-ChocolateyPackage @packageArgs
