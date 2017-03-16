4c7250d060e362d891140327c19e1d3efa631619
Matthieu Penant <thieum22@hotmail.com>
2017-03-16 10:49:25 -0400

    vld 2.5.1

--- visualleakdetector/tools/chocolateyInstall.ps1
+++ visualleakdetector/tools/chocolateyInstall.ps1
@@ -1,7 +1,18 @@
-﻿$packageName = 'visualleakdetector' 
-$installerType = 'EXE'
-$url = 'http://vld.codeplex.com/downloads/get/670164' # download url
-$silentArgs = '/S' 
-$validExitCodes = @(0)
+﻿$ErrorActionPreference = 'Stop';
+$packageName = 'visualleakdetector'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://vld.codeplex.com/downloads/get/1632757' 
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE' 
+  url           = $url
+  softwareName  = 'visualleakdetector'
+  checksum      = '6E9504920F81AED1CF52674BD512BBEF3CD8A1B8A860CAB0FE03C6F19D7008B3'
+  checksumType  = 'sha256' 
+  silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /CLOSEAPPLICATIONS /NORESTART' 
+  validExitCodes = @(0)
+} 
+ 
+Install-ChocolateyPackage @packageArgs
