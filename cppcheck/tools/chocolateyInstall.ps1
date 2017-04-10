127fc7f7edb7c23d0002800ea37cfc146720d888
Matthieu Penant <thieum22@hotmail.com>
2017-04-10 11:36:46 -0400

    cppcheck 1.78.0

--- cppcheck/tools/chocolateyInstall.ps1
+++ cppcheck/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 $packageName = 'cppcheck'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://github.com/danmar/cppcheck/releases/download/1.77/cppcheck-1.77-x86-Setup.msi' 
-$url64 = 'https://github.com/danmar/cppcheck/releases/download/1.77/cppcheck-1.77-x64-Setup.msi' 
+$url = 'https://github.com/danmar/cppcheck/releases/download/1.78/cppcheck-1.78-x86-Setup.msi' 
+$url64 = 'https://github.com/danmar/cppcheck/releases/download/1.78/cppcheck-1.78-x64-Setup.msi' 
 
 $packageArgs = @{
   packageName   = $packageName
@@ -11,9 +11,9 @@ $packageArgs = @{
   url           = $url
   url64bit      = $url64
   softwareName  = 'cppcheck'
-  checksum      = '95803829B87BE9D536672A9523A9945BAF438227D463F25CB75EABC8799A21A1'
+  checksum      = 'ABC347ECE322B86BB879F295C36DBA54C7C06B3DB6596259295CD5C16E6CA1C8'
   checksumType  = 'sha256' 
-  checksum64    = '81EDECA05D3D2CF4BDF469B883413015536467CFB8024D17B88639D60282DF63'
+  checksum64    = '1CCC33EFC0854EA143CF810E2DE95C7D033590EC31A3958BEA70998BCD6ACA1C'
   checksumType64= 'sha256' 
   silentArgs = '/quiet' 
   validExitCodes = @(0)
