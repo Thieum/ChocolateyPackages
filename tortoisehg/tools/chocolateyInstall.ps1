c34ac1b7ad023c7e29ecafca7f596408ce0d5f8f
Matthieu Penant <matthieu.penant@seika.ca>
2019-04-15 11:47:27 -0400

    TortoiseHG 4.9.1

--- tortoisehg/tools/chocolateyInstall.ps1
+++ tortoisehg/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $packageName = 'tortoisehg'
 $installerType = 'msi'
 $silentArgs = '/quiet /qn /norestart'
-$url64 = 'https://bitbucket.org/tortoisehg/files/downloads/tortoisehg-4.9.0-x64.msi'
-$checksum64 = '426C4CFFFEB933B10C7F3E2C2B13E9D843B0A513AD1762FF17DDA0B57492EC08'
+$url64 = 'https://bitbucket.org/tortoisehg/files/downloads/tortoisehg-4.9.1-x64.msi'
+$checksum64 = 'B30D07A55386603C583AAD9C1BEC4AE3CD8D0749E08CA7994AD38EF534D069CF'
 $checksumType64 = 'sha256'
 $validExitCodes = @(0,3010)
 
