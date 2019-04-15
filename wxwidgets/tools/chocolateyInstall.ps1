5547b2daeb7acd81d8eb53083e42b3e800fa9992
Matthieu Penant <matthieu.penant@seika.ca>
2019-04-15 13:22:21 -0400

    wxwidgets 3.1.2

--- wxwidgets/tools/chocolateyInstall.ps1
+++ wxwidgets/tools/chocolateyInstall.ps1
@@ -1,7 +1,15 @@
 ﻿$packageName = 'wxwidgets' 
 $installerType = 'EXE' 
-$url = 'https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.0/wxMSW-3.1.0-Setup.exe' 
+$url = 'https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.2/wxMSW-3.1.2-Setup.exe'
+$checksum = '85ac5b18de191d9d5504b7106466b21b64e7249d'
+$checksumType = 'sha1' 
 $silentArgs = '/silent' 
 $validExitCodes = @(0) 
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
