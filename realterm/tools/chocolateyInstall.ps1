d6175a97536c9837fe95bbc88927ee61bc1e19fd
Matthieu Penant <thieum22@hotmail.com>
2020-06-08 07:50:11 -0400

    realterm 2.0.0.70 fixes

--- realterm/tools/chocolateyInstall.ps1
+++ realterm/tools/chocolateyInstall.ps1
@@ -1,8 +1,20 @@
-﻿$packageName = 'realterm' 
-$installerType = 'EXE'
-$url = 'https://osdn.net/frs/g_redir.php?m=acc&f=realterm%2FRealterm%2F2.0.0.70%2FRealterm_2.0.0.70_setup.exe' 
-$silentArgs = '/S' 
-$validExitCodes = @(0)
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://osdn.net/frs/g_redir.php?m=acc&f=realterm%2FRealterm%2F2.0.0.70%2FRealterm_2.0.0.70_setup.exe'
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
 
+  softwareName  = 'realterm'
+
+  checksum      = 'A82A7B727EFBFD95668A75DDDF534D623C14F674A14036A8A2E3BC5D97D9F17E'
+  checksumType  = 'sha256'
+
+  silentArgs='/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
