de2ba9de0f6b7f2bf546d936f2e1ba3abfc5499b
Robin Stevens <robin.stevens@scz.be>
2020-07-10 17:29:37 +0200

    Updated tortoisehg to 5.4.1

--- tortoisehg/tools/chocolateyInstall.ps1
+++ tortoisehg/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $packageName = 'tortoisehg'
 $installerType = 'msi'
 $silentArgs = '/quiet /qn /norestart'
-$url64 = 'https://bitbucket.org/tortoisehg/files/downloads/tortoisehg-5.1.0-x64.msi'
-$checksum64 = 'E5616C9DF653F96A86EC3B8273E4EF79358BF479B2C81D55D7B7FE736BBC3D40'
+$url64 = 'https://www.mercurial-scm.org/release/tortoisehg/windows/tortoisehg-5.4.1-x64.msi'
+$checksum64 = 'a3c68394d37acf3501418068f32036751fb0c50e888c143dff71f7d1179b4dcf'
 $checksumType64 = 'sha256'
 $validExitCodes = @(0,3010)
 
