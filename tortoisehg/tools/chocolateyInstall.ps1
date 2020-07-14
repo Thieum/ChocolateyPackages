ae5ced1de3c98c267630bc1a5fa28d97de08b993
Matthieu Penant <thieum22@hotmail.com>
2020-07-14 12:42:00 -0400

    tortoisehg 5.4.1

--- tortoisehg/tools/chocolateyInstall.ps1
+++ tortoisehg/tools/chocolateyInstall.ps1
@@ -1,6 +1,9 @@
 $packageName = 'tortoisehg'
 $installerType = 'msi'
 $silentArgs = '/quiet /qn /norestart'
+$url = 'https://www.mercurial-scm.org/release/tortoisehg/windows/tortoisehg-5.4.1-x86.msi'
+$checksum = '1EC44C9BC4218C859A00A579A19558289DD442EEE7E23A05D80045A3D52E4326'
+$checksumType = 'sha256'
 $url64 = 'https://www.mercurial-scm.org/release/tortoisehg/windows/tortoisehg-5.4.1-x64.msi'
 $checksum64 = 'a3c68394d37acf3501418068f32036751fb0c50e888c143dff71f7d1179b4dcf'
 $checksumType64 = 'sha256'
@@ -9,7 +12,10 @@ $validExitCodes = @(0,3010)
 Install-ChocolateyPackage -PackageName "$packageName" `
                           -FileType "$installerType" `
                           -SilentArgs "$silentArgs" `
+                          -Url "$url" `
                           -Url64bit "$url64" `
                           -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
                           -Checksum64 "$checksum64" `
                           -ChecksumType64 "$checksumType64"
