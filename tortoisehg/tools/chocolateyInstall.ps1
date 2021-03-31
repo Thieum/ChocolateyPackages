d2171fb52030180308305a488af998d6576a90e6
Matthieu Penant <thieum22@hotmail.com>
2021-03-31 14:47:57 -0400

    tortoisehg 5.7.1

--- tortoisehg/tools/chocolateyInstall.ps1
+++ tortoisehg/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 $packageName = 'tortoisehg'
 $installerType = 'msi'
 $silentArgs = '/quiet /qn /norestart'
-$url = 'https://www.mercurial-scm.org/release/tortoisehg/windows/tortoisehg-5.7.0-x86.msi'
-$checksum = '154A0F5171E5D8E92562D36282EA45977AB79865F207A8FB120F13BF67CECE19'
+$url = 'https://www.mercurial-scm.org/release/tortoisehg/windows/tortoisehg-5.7.1-x86.msi'
+$checksum = '02192542A79B8BC4B312F087CD71242AE6A04FE6C47DCF3D6F78E2CD29E725A8'
 $checksumType = 'sha256'
-$url64 = 'https://www.mercurial-scm.org/release/tortoisehg/windows/tortoisehg-5.7.0-x64.msi'
-$checksum64 = '7876FECDD0DFBF84CB956C1FCAA890167FA2A715389BE650B36F4978AD4DD109'
+$url64 = 'https://www.mercurial-scm.org/release/tortoisehg/windows/tortoisehg-5.7.1-x64.msi'
+$checksum64 = '0221163B190641E422144C55A1088F1CF1A104D47C81175EC1597B44BFDB23DD'
 $checksumType64 = 'sha256'
 $validExitCodes = @(0,3010)
 
