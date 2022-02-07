9f66b97907b786bc3b2d8cf14fa84253235bbc73
Matthieu Penant <matthieu.penant@seika.ca>
2022-02-07 13:31:24 -0500

    tortoisehg 6.0.0

--- tortoisehg/tools/chocolateyInstall.ps1
+++ tortoisehg/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 $packageName = 'tortoisehg'
 $installerType = 'msi'
 $silentArgs = '/quiet /qn /norestart'
-$url = 'https://www.mercurial-scm.org/release/tortoisehg/windows/tortoisehg-5.9.2-x86.msi'
-$checksum = '1F1FBCB80B3C323AF9E0ADE7E743167A98169A6ED2165C10B24A58AC9EE78709'
+$url = 'https://www.mercurial-scm.org/release/tortoisehg/windows/tortoisehg-6.0.0-x86.msi'
+$checksum = '0430BA797406EA988CAE3A549B7660B8268DBB2C3E9D5E18E40DC99586924FF9'
 $checksumType = 'sha256'
-$url64 = 'https://www.mercurial-scm.org/release/tortoisehg/windows/tortoisehg-5.9.2-x64.msi'
-$checksum64 = 'B3815A80D073CABBA360F441A22F59FEF6810D712FB7861F0CC89189816C079D'
+$url64 = 'https://www.mercurial-scm.org/release/tortoisehg/windows/tortoisehg-6.0.0-x64.msi'
+$checksum64 = '42FA9FBED83621AFCC3EA4C3DDE1B2A253A336EEB08BB12A1CA0E3820DFA9699'
 $checksumType64 = 'sha256'
 $validExitCodes = @(0,3010)
 
