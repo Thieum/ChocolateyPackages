03bda8b3fb49e123f35c1788db3f4badc69b6ab2
Olli Sivonen <olli.sivonen@supercell.com>
2024-12-20 11:57:07 +0200

    cppcheck 2.16

--- cppcheck/tools/chocolateyInstall.ps1
+++ cppcheck/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $packageName = 'cppcheck'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://github.com/danmar/cppcheck/releases/download/2.11/cppcheck-2.11-x64-Setup.msi'
+$url64 = 'https://github.com/danmar/cppcheck/releases/download/2.16.0/cppcheck-2.16.0-x64-Setup.msi'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -9,7 +9,7 @@ $packageArgs = @{
   fileType      = 'MSI'
   url64bit      = $url64
   softwareName  = 'cppcheck'
-  checksum64    = '55BCE7A8911CCDA48D631204D95C9DC6606A590F161E45593452FEFE750B39C7'
+  checksum64    = 'B4137578B4E76F0A1065D561C99D8A286BB1159BC29A0B1914CC33AFC7A890B0'
   checksumType64= 'sha256'
   silentArgs    = '/quiet ADDLOCAL=CppcheckCore,CLI,GUI,Translations,ConfigFiles,PlatformFiles,PythonAddons,CRT /norestart'
   validExitCodes = @(
