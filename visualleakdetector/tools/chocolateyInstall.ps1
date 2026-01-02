4a694e214a01c8991b834bc49391dc1bedf308d3
Matthieu Penant <matthieu.penant@seika.ca>
2026-01-02 00:21:38 -0500

    visual leak detector 2.5.10

--- visualleakdetector/tools/chocolateyInstall.ps1
+++ visualleakdetector/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $packageName = 'visualleakdetector'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://github.com/Azure/vld/releases/download/v2.5.7/vld-2.5.7-setup.exe' 
+$url = 'https://github.com/Azure/vld/releases/download/v2.5.10/vld-2.5.10-setup.exe' 
 
 $packageArgs = @{
   packageName   = $packageName
@@ -9,7 +9,7 @@ $packageArgs = @{
   fileType      = 'EXE' 
   url           = $url
   softwareName  = 'visualleakdetector'
-  checksum      = '934DAECF7A08DB9ED5C6E9B48B87767FA5A76BFFB7C80FB1ADD07CBF87F6BBDF'
+  checksum      = '6fd3113bb624a8d414a44defb872f38394807edc1af0b503618ee0886f651e1d'
   checksumType  = 'sha256' 
   silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /CLOSEAPPLICATIONS /NORESTART' 
   validExitCodes = @(0)
