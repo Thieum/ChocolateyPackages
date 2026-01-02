fc74a7400131e7f3e2b54f38e1db0cf5e54ba059
Matthieu Penant <matthieu.penant@seika.ca>
2026-01-02 00:09:38 -0500

    wxwidgets 3.3.1

--- wxwidgets/tools/chocolateyInstall.ps1
+++ wxwidgets/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'wxwidgets' 
 $installerType = 'EXE' 
-$url = 'https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.3/wxMSW-3.1.3-Setup.exe'
-$checksum = 'BEE29B7693C14A9A725449B651D11DAF304DAB443D506EC11D864AD8556E1450'
+$url = 'https://github.com/wxWidgets/wxWidgets/releases/download/v3.3.1/wxMSW-3.3.1-Setup.exe'
+$checksum = '54e5a57a9d9275f7077acb50152b733a93ab1f9f08ddbf1d04bacbb4ee0d8e50'
 $checksumType = 'sha256' 
 $silentArgs = '/silent' 
 $validExitCodes = @(0) 
